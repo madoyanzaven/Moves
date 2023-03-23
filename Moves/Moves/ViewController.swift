5c40dbb13ed76f7a1cea15df78eeb24570a04729
Zaven Madoyan <zavenmadoyan@gmail.com>
2023-03-23 15:54:56 +0400

    add project

---
+++ Moves/Moves/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moves
+//
+//  Created by Zaven Madoyan on 23.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
